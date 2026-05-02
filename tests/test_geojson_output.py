2d6c9b05cd87dd590e18efe4555c5e6cbfa7b8c6
Kirill Suhanov <kirillikirill86@gmail.com>
2026-05-02 11:25:08 +0300

    change assert output valid for geojson.io

--- tests/test_geojson_output.py
+++ tests/test_geojson_output.py
@@ -5,4 +5,4 @@ def test_linestring_to_polygon():
     with open("test_islands.json") as file:
         data = json.load(file)
     output = linestring_to_polygon(data["features"])
-    assert output == {'type': 'FeatureCollection', 'features': [{'type': 'Feature', 'properties': {'type': 'a'}, 'geometry': {'type': 'Polygon', 'coordinates': [[[0, 1], [1, 1], [1, 0], [0, 0], [0, 1]]]}}, {'type': 'Feature', 'properties': {'class': 'boundaries'}, 'geometry': {'type': 'Polygon', 'coordinates': [[[0.0, 0.0], [0.0, 1.0], [1.0, 1.0], [1.0, 0.0], [0.0, 0.0]]]}}]}
+    assert output == {"type": "FeatureCollection", "features": [{"type": "Feature", "properties": {"type": "a"}, "geometry": {"type": "Polygon", "coordinates": [[[0, 1], [1, 1], [1, 0], [0, 0], [0, 1]]]}}, {"type": "Feature", "properties": {"class": "boundaries"}, "geometry": {"type": "Polygon", "coordinates": [[[0.0, 0.0], [0.0, 1.0], [1.0, 1.0], [1.0, 0.0], [0.0, 0.0]]]}}]}
